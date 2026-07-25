c9592f0194d6bf7d5ed8f5a6e67402dc91fcd446
Choom9 <niko.meletlidis@icloud.com>
2026-07-25 11:26:11 +0200

    Datenbank und Backend

--- Backend/LaldoServer.js
+++ Backend/LaldoServer.js
@@ -1,1 +1,166 @@
 const einträgeArrayFürB = [];
+
+const express = require("express");
+// Holt die Express Bibliothek und speicher ihren inhalt in express vari
+
+const cors = require("cors");
+
+const expressApp = express();
+// express(), erzeigt eine express anwednung gibt sie zurück, wird dann in app gespeichert 
+// express liefert eine Funktion 
+
+expressApp.use(cors());
+expressApp.use(express.json());
+
+ let saldo = 0
+// Saldo muss außerhalb der Route stehen denn sonst wird es immer wieder auf null gesetzt
+// saldo muss let sein weil später neue Zuordnung (saldoUpdate)
+
+expressApp.post("/entries", (req, res) => {
+  einträgeArrayFürB.push(req.body);
+
+  eintragSpeichern.run(
+  req.body.Betrag,
+  req.body.Von,
+  req.body.An
+);
+// führt die Funktion aus, die Einträge in Datenbank speichert
+
+// Saldo wird berechnet von Nikos Perspektive
+// Muss im Frontend noch umgedreht werden wenn User=Yelva
+
+
+    let letzterBetrag = Number(letzteArrEigsch(einträgeArrayFürB, "Betrag"))
+    let letztesVon = letzteArrEigsch(einträgeArrayFürB, "Von")
+    let letztesAn =letzteArrEigsch(einträgeArrayFürB, "An")
+
+    let richtigerBetrag = betragVorzeichen(letzterBetrag, letztesVon,letztesAn);
+
+    saldo = saldoUpdate(richtigerBetrag, saldo)
+
+  res.send(saldo);
+});
+
+// Wenn jemand eine POST-Anfrage an /entries sendet, füge den gesendeten Inhalt in mein Backend-Array ein, berechne Saldo aus Nikos Perspektive
+// Eintrag vom Server kommt rein
+// post sagt: reagiere auf die Post Anfrage mit der Route entries
+// req = request (anfrage), hier findet der server ibformationen z.b über body
+// res = response, damit antowrtet der server
+// res.send(saldo) = schicke saldo zurück als Antowrt 
+
+
+
+
+
+//Funktions Definitionen 
+
+  let letzteArrEigsch = function (array, arrayEigenschaft) {
+  let richtigeEigsch =  array[array.length-1][arrayEigenschaft]
+    return richtigeEigsch
+}
+//Hole das letzte Element aus dem Array, hole xxx Eigenschaft raus, um zu wissen was "das letzte von, an und letzter Betrag sind"
+// Alten Saldo mit neuem betrag verrechnen ist datensparsamer als ständig alles aus dem array zu berechnen um auf saldo zu kommen
+
+
+let betragVorzeichen = function (betrag, von, an ) {
+    let richtigerBetrag 
+    if (von == "Yelva" && an == "Nikos") {
+        richtigerBetrag = -betrag
+    }
+
+    else if (von == "Yelva" && an =="Beide") {
+        richtigerBetrag = -(betrag/2)
+    }
+
+    else if (von == "Yelva" && an == "Yelva" ) {
+        richtigerBetrag = 0
+    }
+
+    else if (von == "Nikos" && an == "Yelva") {
+        richtigerBetrag = +betrag
+    }
+ 
+    else if (von == "Nikos" && an =="Beide") {
+        richtigerBetrag = +(betrag/2)
+    }
+
+   else if (von == "Nikos" && an == "Nikos") {
+     richtigerBetrag = 0
+   }
+
+    return richtigerBetrag
+}
+// Festlegung des Vorzeichens (aus Nikos Perspektive) des neu dazu gekommenen Betrags 
+
+
+let saldoUpdate = function(richtigerBetrag, altSaldo) {
+    let neuSaldo = altSaldo + richtigerBetrag
+    return neuSaldo
+}
+//Eigentliche Verrechhnung: Alter Saldo mit neuem betrag
+
+
+
+
+server.listen(3000);
+// listen = Methode von Express, startet Server
+// 3000 ist die PortNummer, unter der ist er erreichbar 
+
+
+/* Für mich: Was habe ich installiert und warum? 
+1. Node.js
+2. Express
+3. Cors
+
+Warum 1.? Node ist die Laufzeit für JavaScript außrhalb des Browsers
+Laufzeit = Programm, dass Code tatsächlich ausführt. Bei JS im Browser macht es der browser. JS außerhalb für z.b Server braucht ein eigenes Prorgamm -> NODE
+Node wird gestartet über Terminal mit "node Backend/LaldoServer.js"
+Beendet wird der Server mit Command C
+
+Warum 2.? Express gibt Befehle/Methoden für Server Aufbau.
+Express ist eine Bibliothek: fertiger Code von anderen Entwicklern der mir mein Leben leichter macht
+
+Warum 3.? Auch Bib für Node
+Gibt Methoden um Browser mitzuteilen: Dieses Backend darf mit mir (dem Frontend) kommunzieren obwohl wir unterschiedliche Ports haben. ICh (FrontEnd) darf Anfragen an Backend schicken
+Port = Türnummer für programme auf meinem Computer. Programme die über ein Netzwerk erreichbar sein sollen - Server
+localhost = mein Computer
+localhost kann mehrere Programme haben
+Portnummer sagt welcher lokale server die anfrage bekommen soll
+
+
+*/
+
+// Datenbank
+
+const sqlite = require("node:sqlite");
+// Datenbank Bib sqlite laden
+// require lädt eine bereits runtergeladene Datei in eine datei
+// richtiges herunterladen durch npm install
+// sqlite liefert ein Objekt mit Methoden
+
+const datenbank = new sqlite.DatabaseSync("Backend/laldo.db");
+// Klasse = Bauanleitung für objekte
+// DatabaseSync ist eine klasse von sqlite, muss mir new aufgerufen werden, erzeugt ein neues objekt mit den eiegnscahften der klasse 
+// Datenbank wird dadurch beim satrt des Backend erstellt
+
+datenbank.exec(`
+  CREATE TABLE IF NOT EXISTS entries (
+    id INTEGER PRIMARY KEY,
+    Betrag REAL NOT NULL,
+    Von TEXT NOT NULL,
+    An TEXT NOT NULL
+  )
+`);
+
+//tabelle kommt in datenbank
+// .exec() führe Befehl, der in Klammern, in meiner Datenbank aus
+// Create Table IF not exist = erzeuge eine Tabelle, falss noch keine da ist
+// Jedes ding am anfang definiert eine Spalte, id, betrag..., kommas trennen spalten
+// text Not null ? hier wird text gespeichert, not null = darf nicht fehlen der eintrag
+
+const eintragSpeichern = datenbank.prepare(`
+  INSERT INTO entries (Betrag, Von, An)
+  VALUES (?, ?, ?)
+`);
+
+// prepare() = bereite diesen SQL befehl vor aber führe ihn noch nicht aus
--- Frontend/Laldo.js
+++ Frontend/Laldo.js
@@ -29,113 +29,82 @@ buttonOverlay.addEventListener("click", function () {
 })
 
 
-const einträgeArray = []
+
 // async vor Funktion brauche ich, weil ich await benutze
 // await brauche ich damit die funktion wartet bis server ergebnis da ist
 form.addEventListener("submit", async function(e) {
     
     e.preventDefault(); // brauche ich weil sonst daten direkt an browser und weil sonst seite neu lädt
 
-    const data = new FormData(form)
+    const data = new FormData(form);
 
-    const eintrag = Object.fromEntries(data.entries())
+    const eintrag = Object.fromEntries(data.entries());
 
     // fetch -> Anfrage an Server (HTTP)
     // Post -> man sendet Daten an Server
     // JSON stringify -> Übersetzen von js Objekt in JSON, weil JSON verstanden wird von HTTP
-    await fetch("http://localhost:3000/entries", {
+    // das ergebnis, das was Server zurücksendet wird gespeichert in serverAntwortObjekt
+    // aber serverAntwort enthält das ganze Respone Objekt, wir wollen davon nur text haben
+    const serverAntwortObjekt = await fetch("http://localhost:3000/entries", {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(eintrag)
     });
 
-    const eintragInJson= JSON.stringify(eintrag);
+    const serverAntwortSaldo = await serverAntwortObjekt.json();
+    // Json funktion bringt ein Promise (da kommt was). Der Vorgang ist asynchron? Deswegen awiat
+    // Asynchron = der Vorgang braucht eventuell Zeit. JS kann währenddessen andere Sachen erledigen
+    // await = alles unter der Funktion bleibt wird pausiert bis vorgang fertig; aber alles außerhalb nicht betroffen 
 
-    einträgeArray.push(eintrag);
+
+    console.log(serverAntwortSaldo);
+
+    saldoAngepasstAnUser = saldoAngepassungAnUser(serverAntwortSaldo, user);
     
-    let letzterBetrag = Number(letzteArrEigsch(einträgeArray, "Betrag"))
-    let letztesVon = letzteArrEigsch(einträgeArray, "Von")
-    let letztesAn =letzteArrEigsch(einträgeArray, "An")
+ 
+    saldoText(saldosText, user, saldoAngepasstAnUser)
+    console.log("Saldo= " + saldoAngepasstAnUser)
 
-    let richtigerBetrag
-
-    if (user == "Nikos") {
-    richtigerBetrag = betragVorzeichen(letzterBetrag, letztesVon,letztesAn)
-    }
-
-    else if (user == "Yelva") {
-    richtigerBetrag = -betragVorzeichen(letzterBetrag, letztesVon,letztesAn)
-    }
-    
-
-    
-    saldo = saldoUpdate(richtigerBetrag, saldo)
-    saldoText(saldosText, user, saldo)
-    console.log("Saldo= " + saldo)
-
-    saldoHtml.textContent = saldo
+    saldoHtml.textContent = saldoAngepasstAnUser
     console.log(eintrag)
-    console.log(einträgeArray)
 })
 
 // Betrag Rechner Je nachdem .... an .... muss betrag vorzeichen passen
 // Wenn nikos yelva 10€ schuldet (10€ von yelva an Nikos ) -> SaLdo -10€
+let saldoAngepasstAnUser
 
-let letzteArrEigsch = function (array, arrayEigenschaft) {
-   let richtigeEigsch =  array[array.length-1][arrayEigenschaft]
-    return richtigeEigsch
-}
+let saldoAngepassungAnUser = function (saldo, User) {
 
-let betragVorzeichen = function (betrag, von, an ) {
-    let richtigerBetrag 
-    if (von == "Yelva" && an == "Nikos") {
-        richtigerBetrag = -betrag
-    }
-
-    else if (von == "Yelva" && an =="Beide") {
-        richtigerBetrag = -(betrag/2)
-    }
-
-    else if (von == "Yelva" && an == "Yelva" ) {
-        richtigerBetrag = 0
-    }
-
-    else if (von == "Nikos" && an == "Yelva") {
-        richtigerBetrag = +betrag
-    }
- 
-    else if (von == "Nikos" && an =="Beide") {
-        richtigerBetrag = +(betrag/2)
-    }
-
-   else if (von == "Nikos" && an == "Nikos") {
-     richtigerBetrag = 0
+   if (user == "Yelva") {
+    saldoAngepasstAnUser = -saldo
    }
 
-    return richtigerBetrag
-}
+   else {
+    saldoAngepasstAnUser = saldo
+   }
 
-let saldoUpdate = function(richtigerBetrag, altSaldo) {
-    let neuSaldo = altSaldo + richtigerBetrag
-    return neuSaldo
-}
+    return saldoAngepasstAnUser
+};
 
-let saldoText = function(div, user, saldo) {
 
-    let betragOhneVorzeichen = Math.abs(saldo)
 
-    if (user == "Yelva" && saldo < 0 ) {
+let saldoText = function(div, user, saldoAngepasstAnUser) {
+
+    let betragOhneVorzeichen = Math.abs(saldoAngepasstAnUser)
+
+    if (user == "Yelva" && saldoAngepasstAnUser < 0 ) {
     div.textContent = `Yelva schuldet Nikos ${betragOhneVorzeichen}€`
     }
 
-    else if (user == "Yelva" && saldo > 0 ) {
+    else if (user == "Yelva" && saldoAngepasstAnUser > 0 ) {
     div.textContent = `Nikos schuldet Yelva ${betragOhneVorzeichen}€`
     }
 
-    if (user == "Nikos" && saldo < 0 ){
+    if (user == "Nikos" && saldoAngepasstAnUser < 0 ){
     div.textContent = `Nikos schuldet Yelva ${betragOhneVorzeichen}€`
     }
 
-    else if (user == "Nikos" && saldo > 0 ) {
+    else if (user == "Nikos" && saldoAngepasstAnUser > 0 ) {
     {div.textContent = `Yelva schuldet Nikos ${betragOhneVorzeichen}€`}    
     }
 }
